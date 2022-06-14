181409e8252a8bef3bed8791cb38d8ea68df7f70
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-06-14 13:50:48 +0200

    add public folder

--- experience/Objects/FoodManager.js
+++ experience/Objects/FoodManager.js
@@ -1,13 +1,13 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import foodsData from '../data/foodsData';
 import { DragControls } from 'three/examples/jsm/controls/DragControls'
-
-let camera, scene, renderer;
-let controls, group;
+import SceneBase from '../SceneView/Scene/SceneBase';
 
 export default class FoodManager {
 
     constructor(sceneView) {
+
+        
         this.sceneView = sceneView
     }
 
@@ -30,15 +30,9 @@ export default class FoodManager {
     }
 
     addFood(name, {x, y, z}) {
-        
-        // control
-        
-
         const food = this.ressources.getObjectByName(name).children[0] // Mesh
 
         // food.scale.set(1, 1, 1)
-
-        const controls = new DragControls(food, camera, renderer.domElement)
                 
         food.position.set(x, y, z)
         
