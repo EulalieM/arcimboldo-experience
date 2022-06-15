b95a522772b61c62fa94a900942f3086d30e945c
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-06-15 15:28:46 +0200

    wip face

--- experience/SceneView/SceneView.js
+++ experience/SceneView/SceneView.js
@@ -8,7 +8,7 @@ export default class SceneView extends SceneBase {
         super.init();
 
         // HELPERS
-        // this.setControls();
+        this.setControls();
         // this.setHelpers();
 
         this.addFrame()
